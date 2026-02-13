b3d2307cd35e545580cb81d12e76a5e647612223
indiebreath <indiebreath@proton.me>
2026-02-14 10:42:15 +1100

    Added basic info content

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,17 @@ import "~/assets/css/app.css";
 
 <template>
     <div id="info">
+        <h3>Hi, I'm</h3>
         <h1>IndieBreath</h1>
+
+        <img src="" alt="IndieBreath profile picture." />
+
+        <p>Web Developer</p>
+
+        <div id="socials">
+            <a href="">
+                <img src="" alt="GitHub" />
+            </a>
+        </div>
     </div>
 </template>
