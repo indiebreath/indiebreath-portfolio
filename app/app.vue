7ef216afc74dc9cb4c4482d8de4a9319446a4693
indiebreath <indiebreath@proton.me>
2026-02-14 10:33:06 +1100

    Added gruvbox theme and prettierrc

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
+<script>
+import "~/assets/css/app.css";
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div id="info">
+        <h1>IndieBreath</h1>
+    </div>
 </template>
